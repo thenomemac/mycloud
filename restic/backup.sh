8d2303357e84914926422a8280937a5bc8cf3a49
Josiah Olson <thenomemac@gmail.com>
2018-06-30 21:29:24 -0400

    non iteractive shell

--- restic/backup.sh
+++ restic/backup.sh
@@ -2,4 +2,4 @@
 docker pull restic/restic
 docker run --rm --env-file=/opt/mycloud/restic/.env -v /var/lib/docker/volumes/:/data/volumes:ro -v /opt:/data/opt:ro -h olsonsky restic/restic backup /data &&
 docker run --rm --env-file=/opt/mycloud/restic/.env restic/restic forget --keep-hourly 24 --keep-daily 30 &&
-docker run --rm -it --env-file=/opt/mycloud/restic/.env restic/restic snapshots
+docker run --rm --env-file=/opt/mycloud/restic/.env restic/restic snapshots
