3e9a3d39c32aae1f30305b9fcc64fe36d98a75e4
Josiah Olson <thenomemac@gmail.com>
2018-06-30 21:22:19 -0400

    path correction

--- restic/backup.sh
+++ restic/backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker pull restic/restic
-docker run --rm --env-file=/opt/restic/.env -v /var/lib/docker/volumes/:/data/volumes:ro -v /opt:/data/opt:ro -h olsonsky restic/restic backup /data &&
-docker run --rm --env-file=/opt/restic/.env restic/restic forget --keep-hourly 24 --keep-daily 30 &&
-docker run --rm -it --env-file=/opt/restic/.env restic/restic snapshots
+docker run --rm --env-file=/opt/mycloud/restic/.env -v /var/lib/docker/volumes/:/data/volumes:ro -v /opt:/data/opt:ro -h olsonsky restic/restic backup /data &&
+docker run --rm --env-file=/opt/mycloud/restic/.env restic/restic forget --keep-hourly 24 --keep-daily 30 &&
+docker run --rm -it --env-file=/opt/mycloud/restic/.env restic/restic snapshots
