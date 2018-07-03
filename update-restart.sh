fe7282a024333005374a192e223ec601982823f3
Josiah Olson <thenomemac@gmail.com>
2018-07-02 23:24:18 -0400

    make an update script cron able

--- update-restart.sh
+++ update-restart.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-cd mail
-docker-compose pull
-docker-compose up -d
-cd nextcloud
-docker-compose pull
-docker-compose up -d
+cd mail &&
+docker-compose pull &&
+docker-compose up -d &&
+cd .. &
+cd nextcloud &&
+docker-compose pull &&
+docker-compose up -d &&
+echo "updated all mycloud docker"
