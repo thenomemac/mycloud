d293821b484d3876e4af628f77cf14a884389b38
Josiah Olson <thenomemac@gmail.com>
2018-09-03 15:12:44 -0400

    change backup flags

--- restic/backup.sh
+++ restic/backup.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/bin/bash -ex
 docker pull restic/restic
-docker run --rm --env-file=/opt/mycloud/restic/.env -v /var/lib/docker/volumes/:/data/volumes:ro -v /opt:/data/opt:ro -h olsonsky restic/restic backup /data &&
-docker run --rm --env-file=/opt/mycloud/restic/.env -h olsonsky restic/restic forget --keep-hourly 24 --keep-daily 30 &&
+docker run --rm --env-file=/opt/mycloud/restic/.env -v /var/lib/docker/volumes/:/data/volumes:ro -v /opt:/data/opt:ro --host olsonsky restic/restic backup /data &&
+docker run --rm --env-file=/opt/mycloud/restic/.env --host olsonsky restic/restic forget --keep-hourly 24 --keep-daily 30 &&
 docker run --rm --env-file=/opt/mycloud/restic/.env restic/restic snapshots
